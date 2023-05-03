0c369f4b131f270db6b7467d4bbc419dc8d1e122
udeeshabandara <udeeshabandara@gmail.com>
2023-05-03 08:56:05 +0530

    fix:removed re imported models

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const express = require('express')
 const app = express()
 const dotenv = require('dotenv')
-const FlightRouter = require("./routes/flight.routes");
 const AuthRouter = require("./routes/auth.routes");
 const FlightRouter = require("./routes/flight.routes");
 dotenv.config()
