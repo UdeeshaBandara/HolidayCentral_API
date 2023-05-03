ea87628c2925da78cd8e7be1e9fa9b844047f305
udeeshabandara <udeeshabandara@gmail.com>
2023-05-03 09:05:47 +0530

    feat:secured endpoints using auth middleware

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -7,7 +7,6 @@ exports.checkLogin = (req, res) => {
     db.user.find({username: req.body.username, password: req.body.password}).exec().then(r => {
         const result = r[0];
         if (r != null) {
-            console.log(`users found ${r[0]}`);
             res.status(200).send(JSON.stringify(
                 {
                     result,
@@ -27,20 +26,14 @@ exports.saveNewUser = (req, res) => {
         username: req.body.username,
         password: req.body.password,
         userType: req.body.userType,
-        userExerciseType: req.body.exerciseType,
-        userWeight: req.body.weight,
-        userHeight: req.body.height,
-        userGender: req.body.gender,
-        userExerciseTime: req.body.exerciseTime,
         createdAt: Date.now()
     });
 
     user.save().then(r => {
-        if (r.length != 0) {
-            console.log(`new user registered ${r}`);
+        if (r.length !== 0) {
             res.status(201).send(JSON.stringify(r));
         } else {
-            console.log(`registration error`);
+
             res.status(500).send([]);
         }
     });
@@ -50,10 +43,10 @@ exports.getUser = (req, res) => {
     db.user.find({username: req.jwt.username}).exec().then(r => {
         const result = r[0];
         if (r != null) {
-            console.log(`users found ${r[0]}`);
+
             res.status(200).send(JSON.stringify(result));
         } else {
-            console.log(`no user found`);
+
             res.status(500).send([])
         }
     })
--- routes/flight.routes.js
+++ routes/flight.routes.js
@@ -1,19 +1,24 @@
 const FlightController = require('../controllers/flight.controller')
 const FlightMiddleware = require('../middleware/flight.middleware')
+const AuthMiddleware = require('../middleware/auth.middleware')
 exports.routesConfig = function(app) {
     app.get('/flight', [
+        AuthMiddleware.checkValidJWT,
         FlightController.getAllFlights
     ]);
 
     app.get('/flight/query/param', [
+        AuthMiddleware.checkValidJWT,
         FlightController.getSearchParams
     ]);
 
     app.post('/flight/query', [
+        AuthMiddleware.checkValidJWT,
         FlightMiddleware.validateFlightSearchParams,
         FlightController.searchFlights
     ]);
     app.post('/flight/book', [
+        AuthMiddleware.checkValidJWT,
         FlightMiddleware.validateFlightSave,
         FlightController.saveFlightReservation
     ]);
